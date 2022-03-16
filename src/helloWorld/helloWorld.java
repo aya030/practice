25124f8566f81a922edd0d386ee7bd5106ddf394
github user name <kojinco18.03@gmail.com>
2022-03-16 21:15:33 +0900

    first commit

---
+++ src/helloWorld/helloWorld.java
@@ -0,0 +1,23 @@
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("HelloWorld!");
+
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println("HelloWorld!" + i);
+//		}
+		
+		for (int i = 1; i <= 20; i++) {
+			if(i <= 10) {
+				System.out.println("HelloWorld!" + i);
+			}else {
+				System.out.println("Hello" + i);
+			}
+		}
+		
+	}
+
+}
