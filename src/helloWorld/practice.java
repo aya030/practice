c494268435077173548a05546c9498e209464c59
ayn030 <kojinco18.033@gmail.com>
2022-03-19 16:52:27 +0900

    2nd commit

--- src/helloWorld/practice.java
+++ src/helloWorld/practice.java
@@ -12,29 +12,18 @@ public class practice {
 		/**
 		 * /* 1.HelloWorldを変数に代入して表示する
 		 */
-		String greeting = "Hello World";
-		System.out.println(greeting);
+		HelloWorld();
 
 		/**
 		 * /* 2.1から10までを足した値を計算して表示する
 		 */
-		int sum = 0;
-		for (int i = 1; i <= 10; i++) {
-			sum += i;
-		}
-		System.out.println(sum);
+		Sun();
 
 		/**
 		 * /* 3.3年後の今日が何曜日なのかを計算して表示する
 		 */
 		/* ※LocalDateTimeとDayOfWeekを使う */
-		LocalDateTime day = LocalDateTime.now();
-		/* 今日の曜日 */
-//		day.getDayOfWeek();
-		/* 3日後の曜日 */
-//		day.plusDays(3).getDayOfWeek();
-		/* 3年後の曜日 */
-		System.out.println(day.plusYears(3).getDayOfWeek());
+		Day();
 
 		/**
 		 * /* 4.引数が0なら偽。引数が1なら真と表示する
@@ -47,30 +36,49 @@ public class practice {
 		/**
 		 * /* 5.String型のListを作成して表示する
 		 */
-		List<String> list = new ArrayList<>();
-		list.add("ケーキ");
-		list.add("クッキー");
-		list.add("マカロン");
-
-		for (String s : list) {
-			System.out.println("今日のおやつは「 " + s + " 」です");
-		}
-
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(list.get(i));
-//        }
+		DesertList();
 
 		/**
 		 * /* 5.String型のMapを作成して表示する
 		 */
-		Map<String, String> map = new HashMap<>();
-		map.put("兵庫県", "神戸市");
-		map.put("大阪府", "大阪市");
-		map.put("奈良県", "奈良市");
+		AddressList();
+		
+	}
+	
+	
+	
 
-		for (Map.Entry<String, String> entry : map.entrySet()) {
-			System.out.println(entry.getKey() + " : " + entry.getValue());
+	/**
+	 * /* 1.HelloWorldを変数に代入して表示する
+	 */
+	private static void HelloWorld() {
+		String greeting = "Hello World";
+		System.out.println(greeting);
+	}
+
+	/**
+	 * /* 2.1から10までを足した値を計算して表示する
+	 */
+	private static void Sun() {
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
 		}
+		System.out.println(sum);
+	}
+
+	/**
+	 * /* 3.3年後の今日が何曜日なのかを計算して表示する
+	 */
+	/* ※LocalDateTimeとDayOfWeekを使う */
+	private static void Day() {
+		LocalDateTime day = LocalDateTime.now();
+		/* 今日の曜日 */
+//	day.getDayOfWeek();
+		/* 3日後の曜日 */
+//	day.plusDays(3).getDayOfWeek();
+		/* 3年後の曜日 */
+		System.out.println(day.plusYears(3).getDayOfWeek());
 	}
 
 	/**
@@ -85,5 +93,36 @@ public class practice {
 			System.out.println("0か1を入力してください");
 		}
 	}
-	
+
+	/**
+	 * /* 5.String型のListを作成して表示する
+	 */
+	private static void DesertList() {
+		List<String> list = new ArrayList<>();
+		list.add("ケーキ");
+		list.add("クッキー");
+		list.add("マカロン");
+
+		for (String s : list) {
+			System.out.println("今日のおやつは「 " + s + " 」です");
+		}
+//	       for (int i = 0; i < list.size(); i++) {
+//	            System.out.println(list.get(i));
+//	        }
+	}
+
+	/**
+	 * /* 5.String型のMapを作成して表示する
+	 */
+	private static void AddressList() {
+		Map<String, String> map = new HashMap<>();
+		map.put("兵庫県", "神戸市");
+		map.put("大阪府", "大阪市");
+		map.put("奈良県", "奈良市");
+
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
+	}
+
 }
